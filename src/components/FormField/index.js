996293e8c7ad42959612e88f0a26269933fc1ab8
Williams25 <43673479+Williams25@users.noreply.github.com>
2020-07-30 17:56:14 -0300

    Setup aula 4 finalizada

--- src/components/FormField/index.js
+++ src/components/FormField/index.js
@@ -1,16 +1,111 @@
 import React from 'react'
+import PropTypes from 'prop-types'
+import styled, { css } from 'styled-components'
 
-export default props => {
+const FormFieldWrapper = styled.div`
+  position: relative;
+  textarea {
+    min-height: 150px;
+  }
+  input[type="color"] {
+    padding-left: 56px;
+  }
+`
+const Input = styled.input`
+  background: #53585D;
+  color: #F5F5F5;
+  display: block;
+  width: 100%;
+  height: 57px;
+  font-size: 18px;
+  
+  outline: 0;
+  border: 0;
+  border-top: 4px solid transparent;
+  border-bottom: 4px solid #53585D;
+  
+  padding: 16px 16px;
+  margin-bottom: 45px;
+  
+  resize: none;
+  border-radius: 4px;
+  transition: border-color .3s;
+  
+  &:focus {
+    border-bottom-color: var(--primary);
+  }
+  
+  &:focus:not([type="color"]) + span {
+    transform: scale(.6) translateY(-10px);
+  }
+
+  ${
+    ({ hasValue }) => {
+      return hasValue && css`
+          &:not([type="color"]) + span {
+        transform: scale(.6) translateY(-10px);
+      }`
+    }
+  }
+`
+
+const Label = styled.label`
+
+`
+Label.Text = styled.span`
+  color: #E5E5E5;
+  height: 57px;
+  position: absolute; 
+  top: 0;
+  left: 16px;
+  
+  display: flex;
+  align-items: center;
+  
+  transform-origin: 0% 0%;
+  font-size: 18px;
+  font-style: normal;
+  font-weight: 300;
+  
+  transition: .1s ease-in-out;
+`
+
+const FormField = ({
+  label, type, value, onChange,
+}) => {
+  const isTypeTextArea = type === 'textarea';
+  const tag = isTypeTextArea ? 'textarea' : 'input';
+  const hasValue = Boolean(value.length)
   return (
-    <div className={props.className}>
-      <label>
-        {props.label}
-      </label>
-
-      <input type={props.type}
-        value={props.value}
-        onChange={props.onChange}
-      />
-    </div>
-  )
+    <FormFieldWrapper>
+      <Label>
+        <Input
+          as={tag}
+          type={type}
+          value={value}
+          hasValue={hasValue}
+          name={label}
+          onChange={onChange}
+        />
+        <Label.Text>
+          {label}
+          :
+        </Label.Text>
+      </Label>
+    </FormFieldWrapper>
+  );
 }
+
+FormField.defaultProps = {
+  type: 'text',
+  value: '',
+};
+
+FormField.propTypes = {
+  label: PropTypes.string.isRequired,
+  type: PropTypes.string,
+  value: PropTypes.string,
+  onChange: PropTypes.func.isRequired,
+};
+
+export default FormField
--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -1,8 +1,8 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import PageDefault from '../../../components/PageDefault'
 import ButtonLink from '../../../components/Button/ButtonLink'
 import FormField from '../../../components/FormField'
-import FormTextArea from '../../../components/FormField/FormTextArea'
+import ButtonStyled from '../../../components/Button/ButtonStyled'
 
 export default props => {
   const [nome, setNome] = useState()
@@ -27,33 +27,52 @@ export default props => {
     )
   })
 
+  useEffect(() => {
+    const URL = 'http://localhost:8080/categorias'
+    fetch(URL).then(async (response) => {
+      if (response.ok) {
+        const resposta = await response.json()
+        setCategorias([...resposta])
+        return
+      }
+    }).catch(err => console.log(err))
+  },[])
+
   return (
     <PageDefault>
       <h1>Cadastro de Categoria</h1>
       <form onSubmit={submit}>
 
         <FormField
-          label="Nome: "
+          as="text"
+          label="Nome"
           type="text"
           value={nome}
           onChange={e => setNome(e.target.value)}
         />
 
-        <FormTextArea
-          label="Descrição: "
-          type="text"
+        <FormField
+          as="textarea"
+          label="Descrição"
+          type="textarea"
           value={descricao}
           onChange={e => setDescricao(e.target.value)}
         />
 
         <FormField
-          label="Cor: "
+          as="input"
+          label="Cor"
           type="color"
           value={cor}
           onChange={e => setCor(e.target.value)}
         />
 
-        <button type="button" onClick={() => insereCategorias({ nome, descricao, cor })}>Cadastrar</button>
+        <ButtonStyled
+          onClick={
+            () => insereCategorias({ nome, descricao, cor })
+          }>
+          Cadastrar
+        </ButtonStyled>
       </form>
 
       <ul>
